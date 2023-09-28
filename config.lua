ac1a700707133d90707b698ca7af12b33d571793
RexshackGaming <rexshackuk@gmail.com>
2023-09-28 06:35:58 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.PlayerProps = {}
 
 -- settings
 Config.MenuKeybind          = 'J'
-Config.MaxPropCount         = 40 -- maximum props
+Config.MaxPropCount         = 5 -- maximum props
 Config.MaintenancePerCycle  = 1 -- $ amount for prop maintenance
 Config.PurgeStorage         = true
 Config.BillingCycle         = 1 -- will remove credit every x hour/s
